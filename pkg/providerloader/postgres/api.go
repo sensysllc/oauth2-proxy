ad56deb06151d59c72dc3b008b33e204170e12bf
“imranshifa” <shifaimran750@gmail.com>
2023-04-19 12:56:55 +0500

    feat: CI-304 add more descrptive comments and codeclimate config file to resolve similar code errors
    Signed-off-by: “imranshifa” <“shifaimran750@gmail.com”>

--- pkg/providerloader/postgres/api.go
+++ pkg/providerloader/postgres/api.go
@@ -60,16 +60,23 @@ func NewAPI(conf options.API, rs *RedisStore, proxyPrefix string) error {
 }
 
 // This function handles the api for creating a new config entry after validating
-// inputs
+// inputs in the configStore which is an interface defined for storing providers information
+// for adding multi-tenancy in oauth2-proxy
 func (api *API) CreateHandler(rw http.ResponseWriter, req *http.Request) {
+	// validate before creating new config entry and write error
+	// response in case of error
 	id, providerConf, err := api.validateProviderConfig(req)
 	if err != nil {
 		writeErrorResponse(rw, http.StatusBadRequest, err.Error())
 		return
 	}
 
+	// calling create of config storoptimizationse after validation, which will ensure that
+	// no invalid inputs are transferred to avoid errors and
+	// failures
 	err = api.configStore.Create(req.Context(), id, providerConf)
 	if err != nil {
+		// check if error type is already exists is returned
 		if errors.Is(err, ErrAlreadyExists) {
 			writeErrorResponse(rw, http.StatusConflict, err.Error())
 			return
@@ -78,9 +85,12 @@ func (api *API) CreateHandler(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	// returning status created in case of successful execution
 	rw.WriteHeader(http.StatusCreated)
 }
 
+// This function handles the get api for getting the provider configuration
+// from store based on the ID present in the url
 func (api *API) GetHandler(rw http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
 	id := vars["id"]
@@ -100,6 +110,8 @@ func (api *API) GetHandler(rw http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(rw, providerConf)
 }
 
+// This function handles the delete api to delete a provider config entry
+// from store using its ID given in the url
 func (api *API) DeleteHandler(rw http.ResponseWriter, req *http.Request) {
 	vars := mux.Vars(req)
 	id := vars["id"]
@@ -119,7 +131,10 @@ func (api *API) DeleteHandler(rw http.ResponseWriter, req *http.Request) {
 
 // This function updates config in the primary config store which updates cache
 // as well, for update validation of inputs is also done to avoid failures
+// It is an http handler function defined for update api calls and it ensures
+// to return proper response in case of failure as well
 func (api *API) UpdateHandler(rw http.ResponseWriter, req *http.Request) {
+	// inputs are validated
 	id, data, err := api.validateProviderConfig(req)
 	if err != nil {
 		writeErrorResponse(rw, http.StatusBadRequest, err.Error())
@@ -127,8 +142,11 @@ func (api *API) UpdateHandler(rw http.ResponseWriter, req *http.Request) {
 
 	}
 
+	// update function of configStore interface is called to perform an update
+	// of the vaidated inputs passed in the request
 	err = api.configStore.Update(req.Context(), id, data)
 	if err != nil {
+		// check if error type is error not found
 		if errors.Is(err, ErrNotFound) {
 			writeErrorResponse(rw, http.StatusNotFound, err.Error())
 			return
@@ -137,6 +155,7 @@ func (api *API) UpdateHandler(rw http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	// return status accepted if no error is returned
 	rw.WriteHeader(http.StatusAccepted)
 
 }
--- pkg/providerloader/postgres/configStore.go
+++ pkg/providerloader/postgres/configStore.go
@@ -2,9 +2,8 @@ package postgres
 
 import "context"
 
-// ConfigStore is an interface defined that allows any kind of database or store
-// for saving providers configuration.
-// Defining a generic interface allows for smooth testing as well.
+// This is an interface defined for any store to hold configurations.
+// The config store can of any type like postgres or redis.
 type ConfigStore interface {
 	Create(ctx context.Context, id string, providerConfig []byte) error
 	Update(ctx context.Context, id string, providerConfig []byte) error
@@ -12,9 +11,8 @@ type ConfigStore interface {
 	Delete(ctx context.Context, id string) error
 }
 
-// This is an implementation of ConfigStore interface which is used in testing scenarios.
-// As the name implies it is a fake store and looks for implementation of funcs defined in original
-// interface.
+// An implementation of configStore interface, defined for testing scenarios where
+// one can mock these functions and perform robust testing.
 type fakeConfigStore struct {
 	CreateFunc func(ctx context.Context, id string, providerConfig []byte) error
 	UpdateFunc func(ctx context.Context, id string, providerConfig []byte) error
@@ -22,6 +20,7 @@ type fakeConfigStore struct {
 	DeleteFunc func(ctx context.Context, id string) error
 }
 
+// This function calls any implementation of create defined by fakeConfigStore.
 func (f fakeConfigStore) Create(ctx context.Context, id string, providerConfig []byte) error {
 	if f.CreateFunc != nil {
 		return f.CreateFunc(ctx, id, providerConfig)
@@ -29,6 +28,7 @@ func (f fakeConfigStore) Create(ctx context.Context, id string, providerConfig [
 	return nil
 }
 
+// Function below looks for implementation of Update func and returns nil if not found.
 func (f fakeConfigStore) Update(ctx context.Context, id string, providerConfig []byte) error {
 	if f.UpdateFunc != nil {
 		return f.UpdateFunc(ctx, id, providerConfig)
@@ -36,6 +36,8 @@ func (f fakeConfigStore) Update(ctx context.Context, id string, providerConfig [
 	return nil
 }
 
+// Get implements ConfigStore interface func Get and returns nil if no
+// implementation is found.
 func (f fakeConfigStore) Get(ctx context.Context, id string) (string, error) {
 	if f.GetFunc != nil {
 		return f.GetFunc(ctx, id)
@@ -43,6 +45,8 @@ func (f fakeConfigStore) Get(ctx context.Context, id string) (string, error) {
 	return "", nil
 }
 
+// Delete implements interface for Delete func and return any implemented func
+// else nil.
 func (f fakeConfigStore) Delete(ctx context.Context, id string) error {
 	if f.DeleteFunc != nil {
 		return f.DeleteFunc(ctx, id)
